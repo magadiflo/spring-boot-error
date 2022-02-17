849e839039605badebd3c8db6654ce36633847c9
Martín <magadiflo@gmail.com>
2022-02-17 18:26:53 -0500

    Personalizando error 404 y 500

--- src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
+++ src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
-	
+
 	@GetMapping("/index")
 	public String index() {
-		
-		return "index";		
+		// Como no existe la vista index, lanzará el error 500 y en automático mostrará
+		// la vista 500.html ubicado dentro del directorio error. 
+		//Cualquier error 500 mostrará el archivo /error/500.html
+		return "index";
 	}
 
 }
